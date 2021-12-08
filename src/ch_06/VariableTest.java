fe97f9a6117e0e6f0c6a6769e9850693182da697
Choihyunsu <chs991209@gmail.com>
2021-12-08 18:15:44 +0900

    VariableTest class

---
+++ src/ch_06/VariableTest.java
@@ -0,0 +1,7 @@
+package ch_06;
+
+public class VariableTest {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
---
+++ src/chs_sample/HelloJava.java
@@ -0,0 +1,9 @@
+package chs_sample;
+
+public class HelloJava {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, Java");
+    }
+}

1ccbbf5c0bfd57a3ee56d1ab750441cdc17163c9
Choihyunsu <chs991209@gmail.com>
2021-12-20 16:26:20 +0900

    Commit Test

--- src_ch02/ch_01/Student.java
+++ src_ch02/ch_01/Student.java
@@ -2,6 +2,12 @@ package ch_01;
 
 public class Student {
     public static void main(String[] args) {
-        System.out.println("Intro");
+
+        int studentNumber;
+        String studentName;
+        int majorcode;
+        String majorname;
+        String grade;
+
     }
 }

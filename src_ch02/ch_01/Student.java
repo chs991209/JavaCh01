1aa192d5dcbe3230b40ddcae146fd822d9876876
Choihyunsu <chs991209@gmail.com>
2021-12-20 16:26:36 +0900

    Revert "Commit Test"
    This reverts commit 1ccbbf5c0bfd57a3ee56d1ab750441cdc17163c9.

--- src_ch02/ch_01/Student.java
+++ src_ch02/ch_01/Student.java
@@ -2,12 +2,6 @@ package ch_01;
 
 public class Student {
     public static void main(String[] args) {
-
-        int studentNumber;
-        String studentName;
-        int majorcode;
-        String majorname;
-        String grade;
-
+        System.out.println("Intro");
     }
 }
